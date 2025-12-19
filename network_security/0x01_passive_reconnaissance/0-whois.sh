3ba114f72192f7256a8fe5018b6b5aad1bf26bc5
jdidi422 <rassiljdidi3@gmail.com>
2025-12-19 01:35:59 +0100

    change

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
-whois $1 | awk -F: '/Registrant|Admin|Tech/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); if($2!="") print $1 "," $2}' > $1.csv
+
+domain="$1"
+
+whois "$domain" | awk -F: '
+BEGIN { sec="" }
+
+# detect section
+/^Registrant / { sec="Registrant" }
+/^Admin /      { sec="Admin" }
+/^Tech /       { sec="Tech" }
+
+sec != "" {
+    if ($1 ~ /Name$/ && !name[sec])              name[sec]=$2
+    else if ($1 ~ /Organization$/ && !org[sec]) org[sec]=$2
+    else if ($1 ~ /Street$/ && !street[sec])    street[sec]=$2 " "
+    else if ($1 ~ /City$/ && !city[sec])        city[sec]=$2
+    else if ($1 ~ /State\/Province$/ && !state[sec]) state[sec]=$2
+    else if ($1 ~ /Postal Code$/ && !zip[sec])  zip[sec]=$2
+    else if ($1 ~ /Country$/ && !country[sec])  country[sec]=$2
+    else if ($1 ~ /^Phone$/ && !phone[sec])     phone[sec]=$2
+    else if ($1 ~ /^Fax$/ && !fax[sec])         fax[sec]=$2
+    else if ($1 ~ /Email$/ && !email[sec])      email[sec]=$2
+}
+
+END {
+    split("Registrant Admin Tech", order, " ")
+    for (i = 1; i <= 3; i++) {
+        s = order[i]
+        print s " Name," name[s]
+        print s " Organization," org[s]
+        print s " Street," street[s]
+        print s " City," city[s]
+        print s " State/Province," state[s]
+        print s " Postal Code," zip[s]
+        print s " Country," country[s]
+        print s " Phone," phone[s]
+        print s " Phone Ext:,";
+        print s " Fax," fax[s]
+        print s " Fax Ext:,";
+        print s " Email," email[s]
+    }
+}
+' > "$domain.csv"
+
