9751bd59e953343a97c27c7f81085a70940c7011
jdidi422 <rassiljdidi3@gmail.com>
2025-12-19 01:46:08 +0100

    2

--- network_security/0x01_passive_reconnaissance/2-mx_record.sh
+++ network_security/0x01_passive_reconnaissance/2-mx_record.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# 2-mx_record.sh
-# Usage: ./2-mx_record.sh domain
-
-domain="$1"
-
-# nslookup command to get MX records and filter the output
-nslookup -type=mx "$domain" 2>/dev/null | awk -F'= ' '/mail exchanger/ {print $2}'
+nslookup -type=mx "$1"
+'
 
